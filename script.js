6ede9ea19baac89d93f1388536d6db48fff1847f
GustavoMiguelk <miguelgustavo004@gmail.com>
2025-03-27 15:36:55 -0300

    Projeto Finalizado

--- script.js
+++ script.js
@@ -22,6 +22,17 @@ form.addEventListener("submit", (event) => {
     const min = Number(minNumber.value)
     const max = Number(maxNumber.value)
 
+    // Verificando se os valores são válidos.
+    if(min > max){
+        alert("Insira um valor mínimo que seja superior ao valor máximo.")
+        return
+    }
+    else if(min < 0 || max <= 0 || qtd <= 0)
+    {
+        alert("Insira uma valor válido.")
+        return
+    }
+
     // Sorteando a quantidade de números selecionada.
     for(let i = 0; i < qtd; i++){
         // Gerando números inteiros aleatórios.
@@ -32,24 +43,25 @@ form.addEventListener("submit", (event) => {
         sortResult.classList.add("appear")
 
         // Criando o Span que vai receber o resultado.
-        const resultSpan = document.createElement("span")
+        var resultSpan = document.createElement("span")
 
         // Inserindo o número dentro do Span.
         resultSpan.append(numbers[i])
 
         // Inserindo o span dentro da div de resultados.
         result.append(resultSpan)
-
+    }
         // Limpando os Inputs.
         qtdNumber.value = ""
         minNumber.value = ""
         maxNumber.value = ""
-    }
 })
 
 sortRepeat.addEventListener("click", () => {
     // Removendo os números do array.
     numbers.length = 0
+    numbers = []
+    result.innerHTML = ""
 
     // Volta para o section que sorteia novos números.
     sortNumber.classList.remove("remove")
